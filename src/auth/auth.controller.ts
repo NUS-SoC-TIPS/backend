2b77f6fd7dd647fdd19113e376268b733e81d33c
Hanming Zhu <hanming@huawen.com>
2022-05-03 00:13:51 +0800

    feat(auth): add login route to auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+
+import { AuthService } from './auth.service';
+import { AuthDto } from './dtos';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('login')
+  login(@Body() dto: AuthDto): Promise<{ token: string }> {
+    return this.authService.login(dto);
+  }
+}
