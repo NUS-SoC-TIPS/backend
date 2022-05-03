9a34b0b044ee1038f88bbc35b62ca436aa318139
Hanming Zhu <hanming@huawen.com>
2022-05-03 09:50:15 +0800

    feat: improve security

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 
 import { PrismaService } from './prisma/prisma.service';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
+  // app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
