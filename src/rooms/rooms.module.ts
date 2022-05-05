3bf794a5412d783dab1e4eeb62614e2857960c7e
Hanming Zhu <hanming@huawen.com>
2022-05-05 19:07:32 +0800

    fix(rooms): import notes module

--- src/rooms/rooms.module.ts
+++ src/rooms/rooms.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 
 import { AgoraModule } from '../agora/agora.module';
 import { CodeModule } from '../code/code.module';
+import { NotesModule } from '../notes/notes.module';
 
 import { RoomsController } from './rooms.controller';
 import { RoomsGateway } from './rooms.gateway';
@@ -10,6 +11,6 @@ import { RoomsService } from './rooms.service';
 @Module({
   controllers: [RoomsController],
   providers: [RoomsService, RoomsGateway],
-  imports: [AgoraModule, CodeModule],
+  imports: [AgoraModule, CodeModule, NotesModule],
 })
 export class RoomsModule {}
