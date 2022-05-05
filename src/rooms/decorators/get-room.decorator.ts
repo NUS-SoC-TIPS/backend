e3172cb477ae9cc8c6a197b5ebb0798004e7089f
Hanming Zhu <hanming@huawen.com>
2022-05-05 16:08:11 +0800

    feat(rooms): add guard and decorator for rooms

---
+++ src/rooms/decorators/get-room.decorator.ts
@@ -0,0 +1,13 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+import { ISocket } from '../../interfaces/socket';
+
+export const GetRoom = createParamDecorator(
+  (data: string | undefined, ctx: ExecutionContext): any => {
+    const socket: ISocket = ctx.switchToWs().getClient();
+    if (data) {
+      return socket.room?.[data];
+    }
+    return socket.room;
+  },
+);
---
+++ src/rooms/guards/in-room.guard.ts
@@ -0,0 +1,17 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { WsException } from '@nestjs/websockets';
+
+import { ISocket } from '../../interfaces/socket';
+
+@Injectable()
+export class InRoomGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+    const socket: ISocket = context.switchToWs().getClient();
+    if (socket.room == null) {
+      throw new WsException(
+        'You need to be in a room to perform this operation',
+      );
+    }
+    return true;
+  }
+}
