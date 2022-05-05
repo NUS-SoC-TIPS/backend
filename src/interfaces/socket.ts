729360692a2c577b2bc2da9473fb161adf9afee5
Hanming Zhu <hanming@huawen.com>
2022-05-05 14:56:11 +0800

    feat(interfaces): add room to socket data

--- src/interfaces/socket.ts
+++ src/interfaces/socket.ts
@@ -1,7 +1,8 @@
-import { User } from '@prisma/client';
+import { Room, User } from '@prisma/client';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Socket } from 'socket.io';
 
 export interface ISocket extends Socket {
   user?: User;
+  room?: Room;
 }
