650bc6dd4b0d4630e2978166987a847925bc5432
Hanming Zhu <hanming@huawen.com>
2022-05-23 08:58:38 +0800

    feat(prisma): add user settings

---
+++ prisma/migrations/20220523002549_add_settings/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "Settings" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+    "hasUpdatedName" BOOLEAN NOT NULL,
+    "hasUpdatedPhoto" BOOLEAN NOT NULL,
+
+    CONSTRAINT "Settings_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Settings_userId_key" ON "Settings"("userId");
+
+-- AddForeignKey
+ALTER TABLE "Settings" ADD CONSTRAINT "Settings_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
