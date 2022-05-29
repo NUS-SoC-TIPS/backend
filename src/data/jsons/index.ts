a5009eae2cd135fdf8f235655f91adccf3a6d9a9
Hanming Zhu <hanming@huawen.com>
2022-05-29 15:48:14 +0800

    fix(data): fix typo in path

--- src/data/jsons/index.ts
+++ src/data/jsons/index.ts
@@ -1,4 +1,4 @@
 export { default as adminsJson } from './admins.json';
 export { default as configJson } from './config.json';
-export { default as leetCodeJson } from './leetcode.json';
+export { default as leetCodeJson } from './leetCode.json';
 export { default as windowsJson } from './windows.json';
