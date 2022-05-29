692cedbeb64141a925d034c1436697d9e7f0106a
Hanming Zhu <hanming@huawen.com>
2022-05-29 12:02:12 +0800

    feat(records): add method to find valid records in window

--- src/records/records.service.ts
+++ src/records/records.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { RoomRecord, User } from '@prisma/client';
+import { RoomRecord, User, Window } from '@prisma/client';
 
 import { PrismaService } from '../prisma/prisma.service';
 
@@ -21,6 +21,66 @@ export class RecordsService {
         },
       },
     });
+  }
+
+  /**
+   * Finds valid interviews within this window. Validity is defined as:
+   * - There is a partner in the room.
+   * - The interview lasted for at least 15 minutes.
+   *
+   * @param userId User to find records for.
+   * @param window Window to find records for.
+   * @returns The record together with the partner user.
+   */
+  findValidWithinWindow(
+    userId: string,
+    window: Window,
+  ): Promise<
+    (RoomRecord & {
+      partner: User;
+    })[]
+  > {
+    return this.prismaService.roomRecord
+      .findMany({
+        where: {
+          roomRecordUsers: {
+            some: {
+              userId,
+              // Querying for false handles both general and roleplay interviews
+              isInterviewer: false,
+            },
+          },
+          createdAt: {
+            gte: window.startAt,
+            lte: window.endAt,
+          },
+          duration: {
+            gte: 900000, // 15 minutes in milliseconds
+          },
+        },
+        include: {
+          roomRecordUsers: {
+            include: {
+              user: true,
+            },
+          },
+        },
+        orderBy: {
+          createdAt: 'asc',
+        },
+      })
+      .then((interviews) =>
+        interviews
+          .filter((i) => i.roomRecordUsers.length === 2)
+          .map((i) => {
+            const { roomRecordUsers, ...record } = i;
+            return {
+              ...record,
+              partner: roomRecordUsers.filter((u) => u.user.id !== userId)[0]
+                .user,
+            };
+          }),
+      );
   }
 
   async findPage(
