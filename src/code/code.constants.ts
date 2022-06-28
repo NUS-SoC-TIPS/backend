82eb5b29e6b7c728bb4a5efde625907d6454cb46
Hanming Zhu <hanming@huawen.com>
2022-06-29 01:38:30 +0800

    feat(code): update code constants

--- src/code/code.constants.ts
+++ src/code/code.constants.ts
@@ -1,7 +1,10 @@
 export const CODE_EVENTS = {
-  UPDATE_CODE: 'update_code',
+  CONNECT_YJS: 'connect_yjs',
+  UPDATE_YJS: 'update_yjs',
   UPDATE_LANGUAGE: 'update_language',
 };
 
 export const MESSAGE_SYNC = 0;
 export const MESSAGE_AWARENESS = 1;
+export const MESSAGE_AUTH = 2;
+export const MESSAGE_QUERY_AWARENESS = 3;
