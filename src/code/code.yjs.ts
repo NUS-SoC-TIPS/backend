87beccbf56b66b14fb64c4aeb1874b206d8ad2b6
Hanming Zhu <hanming@huawen.com>
2022-06-29 01:38:56 +0800

    feat(code): rewrite yjs doc

--- src/code/code.yjs.ts
+++ src/code/code.yjs.ts
@@ -1,34 +1,33 @@
 import * as encoding from 'lib0/encoding';
-import * as mutex from 'lib0/mutex';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { Socket } from 'socket.io';
+import { ISocket } from 'src/interfaces/socket';
 import * as awarenessProtocol from 'y-protocols/awareness';
 import * as syncProtocol from 'y-protocols/sync';
 import * as Y from 'yjs';
 
+import { CODE_EVENTS, MESSAGE_AWARENESS, MESSAGE_SYNC } from './code.constants';
 
-
-export class SharedDoc extends Y.Doc {
-  awareness: awarenessProtocol.Awareness;
-  mux = mutex.createMutex();
-  sockets = new Map<Socket, Set<number>>();
+export class YjsDoc extends Y.Doc {
+  public awareness: awarenessProtocol.Awareness;
+  // Maps from socket to a set of controlled user IDs. Delete all user IDs from awareness when this socket is disconnected.
+  public connections = new Map<ISocket, Set<number>>();
 
   constructor(public name: string) {
     super({ gc: true });
     this.awareness = new awarenessProtocol.Awareness(this);
     this.awareness.setLocalState(null);
 
-    const awarenessChangeHandler = (
+    // Set handlers
+    const handleAwarenessUpdate = (
       {
         added,
         updated,
         removed,
       }: { added: number[]; updated: number[]; removed: number[] },
-      socket: Socket | null,
+      socket: ISocket | null,
     ): void => {
       const changedClients = added.concat(updated, removed);
       if (socket !== null) {
-        const connControlledIDs = this.sockets.get(socket);
+        const connControlledIDs = this.connections.get(socket);
         if (connControlledIDs !== undefined) {
           added.forEach((clientID) => {
             connControlledIDs.add(clientID);
@@ -45,46 +44,22 @@ export class SharedDoc extends Y.Doc {
         encoder,
         awarenessProtocol.encodeAwarenessUpdate(this.awareness, changedClients),
       );
-      const buff = encoding.toUint8Array(encoder);
-      this.sockets.forEach((_, c) => {
-        send(this, c, buff);
-      });
+      const message = encoding.toUint8Array(encoder);
+      this.connections.forEach((_, socket) =>
+        socket.emit(CODE_EVENTS.UPDATE_YJS, message),
+      );
     };
+    this.awareness.on('update', handleAwarenessUpdate);
 
-    this.awareness.on('update', awarenessChangeHandler);
-    this.on('update', updateHandler);
+    const handleDocUpdate = (update: Uint8Array): void => {
+      const encoder = encoding.createEncoder();
+      encoding.writeVarUint(encoder, MESSAGE_SYNC);
+      syncProtocol.writeUpdate(encoder, update);
+      const message = encoding.toUint8Array(encoder);
+      this.connections.forEach((_, socket) =>
+        socket.emit(CODE_EVENTS.CONNECT_YJS, message),
+      );
+    };
+    this.on('update', handleDocUpdate);
   }
 }
-
-const updateHandler = (
-  update: Uint8Array,
-  _origin: Socket,
-  doc: SharedDoc,
-): void => {
-  const encoder = encoding.createEncoder();
-  encoding.writeVarUint(encoder, MESSAGE_SYNC);
-  syncProtocol.writeUpdate(encoder, update);
-  const message = encoding.toUint8Array(encoder);
-  doc.sockets.forEach((_, socket) => send(doc, socket, message));
-};
-
-const send = (doc: SharedDoc, socket: Socket, m: Uint8Array): void => {
-  try {
-    socket.emit('some_event', m);
-  } catch (e) {
-    closeConn(doc, socket);
-  }
-};
-
-const closeConn = (doc: SharedDoc, socket: Socket): void => {
-  if (doc.sockets.has(socket)) {
-    const controlledIds = doc.sockets.get(socket);
-    doc.sockets.delete(socket);
-    awarenessProtocol.removeAwarenessStates(
-      doc.awareness,
-      Array.from(controlledIds),
-      null,
-    );
-  }
-  socket.disconnect();
-};
