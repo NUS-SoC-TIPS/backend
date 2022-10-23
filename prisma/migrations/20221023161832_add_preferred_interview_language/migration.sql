7b15dd20698d325fd9b41578b63e730f4c96e4e1
Hanming Zhu <hanming@huawen.com>
2022-10-23 09:41:35 -0700

    fix(prisma): fix migration issues

--- prisma/migrations/20221023161832_add_preferred_interview_language/migration.sql
+++ prisma/migrations/20221023161832_add_preferred_interview_language/migration.sql
@@ -1,8 +1,2 @@
-/*
-  Warnings:
-
-  - Added the required column `preferredInterviewLanguage` to the `Settings` table without a default value. This is not possible if the table is not empty.
-
-*/
 -- AlterTable
-ALTER TABLE "Settings" ADD COLUMN     "preferredInterviewLanguage" "language" NOT NULL;
+ALTER TABLE "Settings" ADD COLUMN     "preferredInterviewLanguage" "language";
--- prisma/migrations/20221023163904_make_preferred_language_nullable/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Settings" ALTER COLUMN "preferredInterviewLanguage" DROP NOT NULL;
