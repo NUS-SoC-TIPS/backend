509a72c5a1c5e57f19f9633ab6a21f6de4d8d5a5
Hanming Zhu <hanming@huawen.com>
2022-12-26 10:38:58 -0800

    feat: configure code controller

--- src/code/code.module.ts
+++ src/code/code.module.ts
@@ -3,10 +3,12 @@ import { Module } from '@nestjs/common';
 import { Judge0Module } from '../judge0/judge0.module';
 import { UsersModule } from '../users/users.module';
 
+import { CodeController } from './code.controller';
 import { CodeGateway } from './code.gateway';
 import { CodeService } from './code.service';
 
 @Module({
+  controllers: [CodeController],
   providers: [CodeGateway, CodeService],
   exports: [CodeService],
   imports: [UsersModule, Judge0Module],
