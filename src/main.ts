5b99d53ae07d070bd30fc088fb9008190579d6d8
Hanming Zhu <hanming@huawen.com>
2022-12-28 11:09:17 -0800

    feat: add log file rotation

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import {
   WinstonModule,
 } from 'nest-winston';
 import * as winston from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file';
 
 import { PrismaService } from './prisma/prisma.service';
 import { AppModule } from './app.module';
@@ -22,6 +23,25 @@ function getWinstonFormat(isConsole: boolean): winston.Logform.Format {
   );
 }
 
+const errorTransport = new DailyRotateFile({
+  level: 'error',
+  filename: 'logs/error-%DATE%.log',
+  datePattern: 'YYYY-MM-DD-HH',
+  zippedArchive: true,
+  maxSize: '20m',
+  maxFiles: '14d',
+  format: getWinstonFormat(false),
+});
+
+const combinedTransport = new DailyRotateFile({
+  filename: 'logs/combined-%DATE%.log',
+  datePattern: 'YYYY-MM-DD-HH',
+  zippedArchive: true,
+  maxSize: '20m',
+  maxFiles: '14d',
+  format: getWinstonFormat(false),
+});
+
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule, {
     cors: {
@@ -29,22 +49,15 @@ async function bootstrap(): Promise<void> {
     },
     logger: WinstonModule.createLogger({
       transports: [
-        new winston.transports.File({
-          filename: 'error.log',
-          level: 'error',
-          format: getWinstonFormat(false),
-        }),
-        new winston.transports.File({
-          filename: 'combined.log',
-          format: getWinstonFormat(false),
-        }),
+        errorTransport,
+        combinedTransport,
         new winston.transports.Console({
           format: getWinstonFormat(true),
         }),
       ],
       exceptionHandlers: [
         new winston.transports.File({
-          filename: 'exceptions.log',
+          filename: 'logs/exceptions.log',
           format: getWinstonFormat(false),
         }),
       ],
