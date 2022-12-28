0635b8c3af508d6b2526d51dfcdb986f108237aa
Hanming Zhu <hanming@huawen.com>
2022-12-28 11:58:50 -0800

    feat(agora): add logging

--- src/agora/agora.module.ts
+++ src/agora/agora.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 
 import { AgoraService } from './agora.service';
 
 @Module({
-  providers: [AgoraService],
+  providers: [AgoraService, Logger],
   exports: [AgoraService],
 })
 export class AgoraModule {}
--- src/agora/agora.service.ts
+++ src/agora/agora.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { RtcRole, RtcTokenBuilder } from 'agora-access-token';
 
@@ -6,15 +6,28 @@ import { AGORA_TOKEN_DURATION } from './agora.constants';
 
 @Injectable()
 export class AgoraService {
-  constructor(private readonly configService: ConfigService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly logger: Logger,
+  ) {}
 
   generateAccessToken(roomId: number, userId: string): string {
+    this.logger.log('Generating Agora access token...', AgoraService.name);
     const agoraAppId = this.configService.get('AGORA_APP_ID');
     const agoraAppCertificate = this.configService.get('AGORA_APP_CERTIFICATE');
     if (agoraAppId == null || agoraAppCertificate == null) {
+      this.logger.error(
+        'Agora app ID or certificate not defined, failed to generate access token.',
+        undefined,
+        AgoraService.name,
+      );
       throw new BadRequestException();
     }
 
+    this.logger.log(
+      'Successfully generated Agora access token!',
+      AgoraService.name,
+    );
     return RtcTokenBuilder.buildTokenWithAccount(
       agoraAppId,
       agoraAppCertificate,
