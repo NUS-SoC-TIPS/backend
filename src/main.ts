f0f0e009fa9f531224470fc034e423908a3cd501
Hanming Zhu <hanming@huawen.com>
2022-12-28 11:04:48 -0800

    feat: add winston to project and upgrade deps

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,53 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
+import {
+  utilities as nestWinstonModuleUtilities,
+  WinstonModule,
+} from 'nest-winston';
+import * as winston from 'winston';
 
 import { PrismaService } from './prisma/prisma.service';
 import { AppModule } from './app.module';
+
+function getWinstonFormat(isConsole: boolean): winston.Logform.Format {
+  return winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.ms(),
+    nestWinstonModuleUtilities.format.nestLike('tips-backend', {
+      colors: isConsole,
+      prettyPrint: isConsole,
+    }),
+  );
+}
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule, {
     cors: {
       origin: process.env.NODE_ENV === 'production' ? /soc-tips\.com$/ : '*',
     },
+    logger: WinstonModule.createLogger({
+      transports: [
+        new winston.transports.File({
+          filename: 'error.log',
+          level: 'error',
+          format: getWinstonFormat(false),
+        }),
+        new winston.transports.File({
+          filename: 'combined.log',
+          format: getWinstonFormat(false),
+        }),
+        new winston.transports.Console({
+          format: getWinstonFormat(true),
+        }),
+      ],
+      exceptionHandlers: [
+        new winston.transports.File({
+          filename: 'exceptions.log',
+          format: getWinstonFormat(false),
+        }),
+      ],
+    }),
   });
   app.use(helmet());
   app.useGlobalPipes(
