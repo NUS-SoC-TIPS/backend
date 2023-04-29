a55fd2405b5d1b53b5fdeda07e9e5c59ed447d20
Hanming Zhu <hanming@huawen.com>
2023-04-29 16:22:48 -0700

    fix(prisma): make user table columns snake case

---
+++ prisma/migrations/20230429231705_make_user_columns_snake_case/migration.sql
@@ -0,0 +1,13 @@
+/*
+ 
+ NOTE: This migration script is manually written to avoid dropping all the below columns.
+ 
+*/
+-- AlterTable
+BEGIN;
+ALTER TABLE "users" RENAME "createdAt" TO "created_at";
+ALTER TABLE "users" RENAME "updatedAt" TO "updated_at";
+ALTER TABLE "users" RENAME "githubUsername" TO "github_username";
+ALTER TABLE "users" RENAME "photoUrl" TO "photo_url";
+ALTER TABLE "users" RENAME "profileUrl" TO "profile_url";
+COMMIT;
