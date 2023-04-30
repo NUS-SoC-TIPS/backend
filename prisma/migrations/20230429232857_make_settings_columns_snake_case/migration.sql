c14e3eca6f8f3633d4fee254eea4868cb36174e9
Hanming Zhu <hanming@huawen.com>
2023-04-29 19:50:49 -0700

    fix(prisma): make settings table columns snake case

---
+++ prisma/migrations/20230429232857_make_settings_columns_snake_case/migration.sql
@@ -0,0 +1,17 @@
+/*
+ 
+ NOTE: This migration script is manually written to avoid dropping all the below columns.
+ 
+*/
+-- AlterTable
+BEGIN;
+ALTER TABLE "settings" RENAME "createdAt" TO "created_at";
+ALTER TABLE "settings" RENAME "updatedAt" TO "updated_at";
+ALTER TABLE "settings" RENAME "userId" TO "user_id";
+ALTER TABLE "settings" RENAME "hasUpdatedName" TO "has_updated_name";
+ALTER TABLE "settings" RENAME "hasUpdatedPhoto" TO "has_updated_photo";
+ALTER TABLE "settings" RENAME "preferredInterviewLanguage" TO "preferred_interview_language";
+ALTER TABLE "settings" RENAME "preferredKeyBinding" TO "preferred_key_binding";
+ALTER TABLE "settings" RENAME CONSTRAINT "settings_userId_fkey" TO "settings_user_id_fkey";
+ALTER INDEX "settings_userId_key" RENAME TO "settings_user_id_key";
+COMMIT;
